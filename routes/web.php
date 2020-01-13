e1fabdcaf51f79dacfabbf952110be1711eba701
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2020-01-13 19:54:27 +0300

    WIP: Login system
    First version of login system with roles

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/users','UsersController',['except' => ['show','create','store']]);
+
+
+
+});
