a4effe87ebf79d6cec739d76e70f8d9f1fc07c9d
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2020-01-15 00:08:43 +0300

    WIP : Books

---
+++ app/Book.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    public function add($name,$author,$image_url,$owner_id,$temp_owner_id){
+
+    }
+    public function takeBook($temp_owner_id, $id_books)
+    {
+
+    }
+    public function searchBook($bookName)
+    {
+
+    }
+
+}
